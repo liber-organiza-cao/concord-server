ff0b0685c13a9b68853bb60c7429b31e9923ee54
Heráclito <heraclitoqsaldanha@gmail.com>
2024-10-23 00:11:26 -0300

    refatoração

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,33 @@
 use nanoserde::{DeJson, SerJson};
-use std::collections;
-use std::io::ErrorKind;
-use std::net;
-use std::sync;
-use std::thread;
-use std::time;
 
-mod client;
-mod orchestrator;
+mod server;
 
-#[derive(DeJson, SerJson, Clone, PartialEq, Eq)]
-pub enum NetworkMessage {
+#[derive(Debug, Clone, PartialEq, Eq, DeJson, SerJson)]
+pub enum Message {
 	SendMessage { channel: String, content: String },
-	ReceiveMessage { author: u32, channel: String, content: String },
-	Registered { id: u32 },
+	ReceiveMessage { author: u64, channel: String, content: String },
 }
-
-#[derive(Debug, Clone)]
-pub enum InternalMessage {
-	Register(Sender),
-	Registered { id: u32 },
-	Unregister { id: u32 },
-	SendMessage { author: u32, channel: String, content: String },
-}
-
-pub type Websocket = tungstenite::WebSocket<net::TcpStream>;
-pub type Receiver = sync::mpsc::Receiver<InternalMessage>;
-pub type Sender = sync::mpsc::Sender<InternalMessage>;
-
-const TIMEOUT: time::Duration = time::Duration::from_millis(100);
 
 fn main() {
 	#[cfg(not(debug_assertions))]
 	simple_logger::init_with_level(log::Level::Info).unwrap();
 	#[cfg(debug_assertions)]
 	simple_logger::init_with_level(log::Level::Debug).unwrap();
-	let server = net::TcpListener::bind("0.0.0.0:6464").unwrap();
-	let (sender, receiver) = sync::mpsc::channel::<InternalMessage>();
-	thread::spawn(move || orchestrator::handler(receiver));
-	for stream in server.incoming() {
-		let Ok(stream) = stream else {
-			continue;
-		};
-		stream.set_read_timeout(Some(TIMEOUT)).unwrap();
-		let Ok(websocket) = tungstenite::accept(stream) else {
-			continue;
-		};
-		let sender = sender.clone();
-		thread::spawn(move || client::handler(websocket, sender));
-	}
+
+	let server = server::Server::<Message, 5>::bind("0.0.0.0:6464").unwrap();
+	server.run(&|handler| {
+		if let Some((id, msg)) = handler.read() {
+			match msg {
+				Message::SendMessage { channel, content } => {
+					let author = id;
+					for id in handler.get_clients() {
+						let channel = channel.clone();
+						let content = content.clone();
+						handler.send(id, Message::ReceiveMessage { author, channel, content }).unwrap();
+					}
+				}
+				_ => {}
+			}
+		}
+	});
 }
---
+++ src/server.rs
@@ -0,0 +1,118 @@
+use std::io;
+use std::marker;
+use std::net;
+use std::thread;
+use std::time;
+
+enum InternalMessage<T: nanoserde::DeJson + nanoserde::SerJson> {
+	SendMessage { receiver_id: u64, message: T },
+}
+
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+	Io,
+	SendError,
+}
+
+pub struct Server<T: nanoserde::DeJson + nanoserde::SerJson, const TIMEOUT_IN_MILLIS: u64> {
+	listener: net::TcpListener,
+	message: marker::PhantomData<T>,
+}
+
+pub struct ConnectionHandler<T: nanoserde::DeJson + nanoserde::SerJson> {
+	sender: crossbeam_channel::Sender<InternalMessage<T>>,
+	messages_queue: Vec<(u64, T)>,
+}
+
+impl<T: nanoserde::DeJson + nanoserde::SerJson> ConnectionHandler<T> {
+	pub fn send(&mut self, id: u64, message: T) -> Result<()> {
+		Ok(self.sender.send(InternalMessage::SendMessage { receiver_id: id, message })?)
+	}
+	pub fn read(&mut self) -> Option<(u64, T)> {
+		self.messages_queue.pop()
+	}
+	pub fn get_clients(&self) -> Vec<u64> {
+		Vec::new()
+	}
+}
+
+impl<T: nanoserde::DeJson + nanoserde::SerJson + std::marker::Send + 'static, const TIMEOUT_IN_MILLIS: u64> Server<T, TIMEOUT_IN_MILLIS> {
+	pub fn bind<A: net::ToSocketAddrs>(addr: A) -> Result<Self> {
+		let listener = net::TcpListener::bind(addr)?;
+		Ok(Self {
+			listener,
+			message: marker::PhantomData,
+		})
+	}
+	pub fn run<F: Fn(&mut ConnectionHandler<T>) + std::marker::Sync>(self, handshake: &'static F) {
+		let timeout = time::Duration::from_millis(TIMEOUT_IN_MILLIS);
+		let (sender, receiver) = crossbeam_channel::bounded::<InternalMessage<T>>(0);
+		let mut id_counter = 0;
+		for stream in self.listener.incoming() {
+			let Ok(stream) = stream else {
+				continue;
+			};
+			stream.set_read_timeout(Some(timeout)).unwrap();
+			let Ok(mut websocket) = tungstenite::accept(stream) else {
+				continue;
+			};
+
+			let sender = sender.clone();
+			let receiver = receiver.clone();
+			let id = id_counter;
+
+			let mut handler = ConnectionHandler {
+				sender,
+				messages_queue: Vec::new(),
+			};
+			id_counter += 1;
+			thread::spawn(move || loop {
+				match websocket.read() {
+					Ok(tungstenite::Message::Text(str)) => {
+						if let Ok(msg) = T::deserialize_json(&str) {
+							handler.messages_queue.push((id, msg));
+						}
+					}
+					Err(tungstenite::Error::AlreadyClosed | tungstenite::Error::ConnectionClosed) => break,
+					Err(tungstenite::Error::Io(err)) => {
+						let err = err.kind();
+						if !matches!(err, io::ErrorKind::WouldBlock) {
+							log::error!("{err}");
+						}
+					}
+					_ => {}
+				};
+
+				match receiver.recv_timeout(timeout) {
+					Ok(InternalMessage::SendMessage { receiver_id, message }) => {
+						if receiver_id == id {
+							let message = message.serialize_json();
+							if let Err(e) = websocket.send(tungstenite::Message::text(message)) {
+								log::error!("{e}");
+							}
+						}
+					}
+					Err(crossbeam_channel::RecvTimeoutError::Timeout) => {}
+					Err(e) => log::error!("{e}"),
+				}
+
+				handshake(&mut handler);
+			});
+		}
+	}
+}
+
+impl From<io::Error> for Error {
+	#[inline(always)]
+	fn from(_: io::Error) -> Self {
+		Self::Io
+	}
+}
+
+impl<T> From<crossbeam_channel::SendError<T>> for Error {
+	fn from(_: crossbeam_channel::SendError<T>) -> Self {
+		Self::SendError
+	}
+}
