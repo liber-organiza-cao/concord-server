ba010830509031636f5db096698ca4c549993cd1
Heráclito <heraclitoqsaldanha@gmail.com>
2024-12-11 23:57:01 -0300

    puta

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,42 @@
+use std::collections;
+
 mod error;
 mod server;
 
 #[derive(Debug, Clone, PartialEq, Eq, serde::Deserialize, serde::Serialize)]
 pub enum Message {
-	SendMessage {
-		channel: String,
-		content: String,
-	},
-	ReceiveMessage {
-		author: u64,
-		channel: String,
-		content: String,
-	},
-	Connected {
-		id: u64,
-	},
-	Disconnected {
-		id: u64,
-	},
-	ChangeStatus {
-		author: u64,
-		afk: bool,
-	},
-	Offer {
-		r#type: String,
-		sdp: String,
-	},
-	Answer {
-		r#type: String,
-		sdp: String,
-	},
-	Candidate {
-		candidate: Option<String>,
-		sdpMLineIndex: Option<i64>,
-		sdpMid: Option<String>,
-		usernameFragment: Option<String>,
-	},
+	SendMessage { channel: String, content: String },
+	ReceiveMessage { author: u64, channel: String, content: String },
+	Connected { id: u64 },
+	Disconnected { id: u64 },
+	ChangeStatus { author: u64, afk: bool },
+	Offer { id: u64, data: Offer },
+	Answer { id: u64, data: Answer },
+	Candidate { id: u64, data: Candidate },
+	JoinVoiceChannel { channel: String },
+	JoinedVoiceChannel { channel: String, id: u64 },
+	LeaveVoiceChannel { channel: String },
+	LeftVoiceChannel { channel: String, id: u64 },
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Deserialize, serde::Serialize)]
+struct Answer {
+	r#type: String,
+	sdp: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Deserialize, serde::Serialize)]
+struct Offer {
+	r#type: String,
+	sdp: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, serde::Deserialize, serde::Serialize)]
+struct Candidate {
+	candidate: Option<String>,
+	sdpMLineIndex: Option<i64>,
+	sdpMid: Option<String>,
+	usernameFragment: Option<String>,
 }
 
 fn main() {
@@ -45,13 +46,40 @@ fn main() {
 	simple_logger::init_with_level(log::Level::Debug).unwrap();
 
 	let mut server = server::Server::<Message, 5>::bind("0.0.0.0:6464").unwrap();
+	let mut voice_channel_peers = collections::HashMap::<String, Vec<u64>>::new();
+
 	while server.pull() {
 		if let Some(id) = server.on_connected() {
 			let _ = server.send(id, Message::Connected { id });
+			for (channel, peers) in &voice_channel_peers {
+				for peer_id in peers {
+					let channel = channel.clone();
+					let _ = server.send(id, Message::JoinedVoiceChannel { channel, id: *peer_id });
+				}
+			}
 			log::info!("{id} connected");
 		}
 		if let Some(disconnected_id) = server.on_disconnected() {
 			log::info!("{disconnected_id} disconnected");
+
+			for (channel, peers) in &mut voice_channel_peers {
+				peers.retain(|p| {
+					let state = *p != disconnected_id;
+					if !state {
+						for client_id in server.get_clients() {
+							let channel = channel.clone();
+							let _ = server.send(
+								client_id,
+								Message::LeftVoiceChannel {
+									channel,
+									id: disconnected_id,
+								},
+							);
+						}
+					}
+					state
+				});
+			}
 
 			for id in server.get_clients() {
 				let _ = server.send(id, Message::Disconnected { id: disconnected_id });
@@ -72,52 +100,37 @@ fn main() {
 						let _ = server.send(id, Message::ChangeStatus { author, afk });
 					}
 				}
-				Message::Offer { r#type, sdp } => {
-					let author = id;
-					for id in server.get_clients() {
-						if author == id {
+				Message::Offer { id: receiver_id, data } => {
+					let _ = server.send(receiver_id, Message::Offer { id, data });
+				}
+				Message::Candidate { id: receiver_id, data } => {
+					let _ = server.send(receiver_id, Message::Candidate { id, data });
+				}
+				Message::Answer { id: receiver_id, data } => {
+					let _ = server.send(receiver_id, Message::Answer { id, data });
+				}
+				Message::JoinVoiceChannel { channel } => {
+					if let Some(peers) = voice_channel_peers.get_mut(&channel) {
+						if peers.contains(&id) {
 							continue;
 						}
-						let r#type = r#type.clone();
-						let sdp = sdp.clone();
-						let _ = server.send(id, Message::Offer { r#type, sdp });
+						peers.push(id);
+						for client_id in server.get_clients() {
+							let channel = channel.clone();
+							let _ = server.send(client_id, Message::JoinedVoiceChannel { channel, id });
+						}
+					} else {
+						let peers = vec![id];
+						voice_channel_peers.insert(channel, peers);
 					}
 				}
-				Message::Candidate {
-					candidate,
-					sdpMLineIndex,
-					sdpMid,
-					usernameFragment,
-				} => {
-					let author = id;
-					for id in server.get_clients() {
-						if author == id {
-							continue;
-						}
-						let candidate = candidate.clone();
-						let sdpMLineIndex = sdpMLineIndex.clone();
-						let sdpMid = sdpMid.clone();
-						let usernameFragment = usernameFragment.clone();
-						let _ = server.send(
-							id,
-							Message::Candidate {
-								candidate,
-								sdpMLineIndex,
-								sdpMid,
-								usernameFragment,
-							},
-						);
+				Message::LeaveVoiceChannel { channel } => {
+					if let Some(peers) = voice_channel_peers.get_mut(&channel) {
+						peers.retain(|peer_id| *peer_id != id);
 					}
-				}
-				Message::Answer { r#type, sdp } => {
-					let author = id;
-					for id in server.get_clients() {
-						if author == id {
-							continue;
-						}
-						let r#type = r#type.clone();
-						let sdp = sdp.clone();
-						let _ = server.send(id, Message::Answer { r#type, sdp });
+					for peer_id in server.get_clients() {
+						let channel = channel.clone();
+						let _ = server.send(peer_id, Message::LeftVoiceChannel { channel, id });
 					}
 				}
 				_ => {}
